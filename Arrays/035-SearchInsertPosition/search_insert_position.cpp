ef4b14396308fa714eba1ef3ad4c1d49ebfff742
Manan Jani <manan.jani.cg@gmail.com>
2026-05-17 11:00:00 +0530

    feat: solve Search Insert Position problem

---
+++ Arrays/035-SearchInsertPosition/search_insert_position.cpp
@@ -0,0 +1,42 @@
+// LeetCode 35 - Search Insert Position
+// Approach: Binary Search
+// Time: O(log n) | Space: O(1)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {1, 3, 5, 6};
+
+    cout << sol.searchInsert(nums, 5) << endl; // 2
+    cout << sol.searchInsert(nums, 2) << endl; // 1
+    cout << sol.searchInsert(nums, 7) << endl; // 4
+
+    return 0;
+}
